9c8228126b455b7182af3efed3ebdd95d461cc3f
JM <jnmrles@gmail.com>
2021-02-24 14:57:16 -0600

    updated styling

--- src/Banner.js
+++ src/Banner.js
@@ -3,11 +3,15 @@ import React from "react";
 import "./Banner.css";
 
 function Banner() {
+  function truncate(string, end) {
+    return string?.length > end ? string.substr(0, end - 1) + "..." : string;
+  }
+
   return (
     <header
       className="banner"
       style={{
-        backgroundImage: `url('https://i.imgur.com/e1hLQ2m.png')`,
+        backgroundImage: `url('https://i.pinimg.com/736x/62/cb/b8/62cbb8138fd6c38a72198e09859a5426.jpg')`,
         backgroundSize: "cover",
         backgroundPosition: "center center",
       }}
@@ -15,10 +19,17 @@ function Banner() {
       <div className="banner__contents">
         <h1 className="banner__title">Movie Name</h1>
         <div className="banner__buttons">
-          <button>Play</button>
-          <button>My List</button>
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
         </div>
+        <h1 className="banner__description">
+          {truncate(
+            "This is the banner description This is the banner description This is the banner description This is the banner description This is the banner description This is the banner description This is the banner description This is the banner description This is the banner description This is the banner description This is the banner description This is the banner description This is the banner description This is the banner description ",
+            150
+          )}
+        </h1>
       </div>
+      <div className="banner--fadeBottom"></div>
     </header>
   );
 }
