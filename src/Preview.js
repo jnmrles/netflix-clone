99820d92291e64b4d3cd78c6463bb5d78e975ecc
JM <jnmrles@gmail.com>
2021-02-25 17:32:05 -0600

    styled preview component. updated movie selector styling

--- src/Preview.js
+++ src/Preview.js
@@ -8,24 +8,36 @@ function Preview(props) {
 
   return closed === false ? (
     <>
-      <div className="preview">
-        <div className="preview__background">
-          <div className="preview__background__shadow"></div>
-          <img
-            className="preview__image"
-            src={`${base_url}${
+      <section class="section">
+        <div class="content">
+          <h2 class="title">This is a title</h2>
+          <p class="paragraph">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. At non ex
+            earum, libero dignissimos voluptates. Quis beatae dolorem autem
+            ipsa!
+          </p>
+        </div>
+        {/* <img
+          className="image"
+          key={movie.id}
+          src={`${base_url}${
+            movie.poster_path ? movie.poster_path : movie.backdrop_path
+          }`}
+          alt={movie.name}
+        /> */}
+        <div className="banner--fadeBottom"></div>
+        <div
+          className="preview__image"
+          style={{
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${
               movie.poster_path ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
-          ></img>
-        </div>
-
-        <div className="preview__details">
-          <div className="details_container">
-            <h1 className="details__title">NAME</h1>
-          </div>
-        </div>
-      </div>
+            }")`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRpeat: "no-repeat",
+          }}
+        ></div>
+      </section>
     </>
   ) : null;
 }
