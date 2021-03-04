f6c382f2734f680d09af02f8a34ec483235ba476
JM <jnmrles@gmail.com>
2021-03-04 03:44:43 -0600

    fixed bug with useDispatch hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
 import HomeScreen from "./HomeScreen";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./Login";
 import { auth } from "./firebase";
+import { login, logout } from "./features/userSlice";
 function App() {
   const user = null;
+  const dispatch = useDispatch();
+
   useEffect(() => {
     const unsubsrube = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
         console.log(userAuth);
+        dispatch(login({ uid: userAuth.uid, email: userAuth.email }));
       } else {
+        dispatch(logout);
       }
 
       return unsubsrube;
