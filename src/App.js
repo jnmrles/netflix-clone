971bc6894701067bbc72b380f73251e50066ac5a
JM <jnmrles@gmail.com>
2021-03-04 02:50:37 -0600

    added useEffec react hook to implements firebase auth persistance

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./HomeScreen";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./Login";
+import { auth } from "./firebase";
 function App() {
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        console.log(userAuth);
+      } else {
+      }
+    });
+  }, []);
   return (
     <div className="app">
       <Router>
